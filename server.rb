5f8fc49326f54e847b5b98acc460db8be5bb9eec
Ben Balter <ben.balter@github.com>
2015-02-04 13:59:01 -0500

    simplify

--- server.rb
+++ server.rb
@@ -13,32 +13,24 @@ class IsPennsylvaniaAvenueOpen < Sinatra::Base
 
   configure do
     uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:16379")
-    @@redis = Redis.new(
-      :host     => uri.host,
-      :port     => uri.port,
-      :password => uri.password
-    )
+    @@redis = Redis.new(:host => uri.host, :port => uri.port,:password => uri.password)
   end
 
   def redis
     @@redis
   end
 
-  def set(value)
-    redis.set "closed", value
-    redis.set "timestamp", Time.now.to_i
-  end
-
   def closed?
-    redis.get("closed") == "true"
+    @closed ||= redis.get("closed") == "true"
   end
 
   def timestamp
-    Time.at(redis.get("timestamp").to_i)
+    @timestamp ||= Time.at(redis.get("timestamp").to_i)
   end
 
   post "/update" do
-    set !closed?
+    redis.set "closed", !closed?
+    redis.set "timestamp", Time.now.to_i
     redirect "/"
   end
 
@@ -47,9 +39,6 @@ class IsPennsylvaniaAvenueOpen < Sinatra::Base
   end
 
   get "/api" do
-    jsonp({
-      :closed => closed?,
-      :as_of  => timestamp
-    })
+    jsonp({:closed => closed?, :timestamp => timestamp})
   end
 end
