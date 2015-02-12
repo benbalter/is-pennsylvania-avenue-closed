24e1218b7a03d02aebd9d78d1cedc513a9da5941
Ben Balter <ben.balter@github.com>
2015-02-11 20:02:57 -0500

    fix travis

--- lib/redis_helper.rb
+++ lib/redis_helper.rb
@@ -1,7 +1,7 @@
 class IsPennsylvaniaAvenueClosed < Sinatra::Base
   module RedisHelper
     def init_redis!
-      uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:16379")
+      uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://127.0.0.1:6379")
       @@redis = Redis.new(:host => uri.host, :port => uri.port,:password => uri.password)
     end
   end
